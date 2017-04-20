b90f554c8bf9b621ad7721c5e9265f0301862145
HenryDev <onyxorca@gmail.com>
2017-04-19 21:41:17 -0500

    test resultsπ

--- source/pipeline_image.py
+++ source/pipeline_image.py
@@ -1,4 +1,6 @@
 import cv2
+import glob
+
 import numpy as np
 
 from source.converter import yolo_net_out_to_car_boxes
@@ -11,21 +13,12 @@ model = make_model()
 
 load_weights(model, '../yolo-tiny.weights')
 
-imagePath = '../test_images/test1.jpg'
-image = plt.imread(imagePath)
-image_crop = image[300:650, 500:, :]
-resized = cv2.resize(image_crop, (448, 448))
-
-batch = np.transpose(resized, (2, 0, 1))
+images = [plt.imread(file) for file in glob.glob('../test_images/*.jpg')]
+batch = np.array([np.transpose(cv2.resize(image[300:650, 500:, :], (448, 448)), (2, 0, 1)) for image in images])
 batch = 2 * (batch / 255.) - 1
-batch = np.expand_dims(batch, axis=0)
-out = model.predict(batch)
-
-boxes = yolo_net_out_to_car_boxes(out[0], threshold=0.17)
-
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
-ax1.imshow(image)
-box = draw_box(boxes, plt.imread(imagePath), [[500, 1280], [300, 650]])
-ax2.imshow(box)
-# cv2.imwrite('../output_images/' + 'test1 result.jpg', box)
-plt.imsave('../output_images/' + 'test1 result.jpg', box)
+prediction = model.predict(batch)
+f, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2, figsize=(11, 10))
+for i, ax in zip(range(len(batch)), [ax1, ax2, ax3, ax4, ax5, ax6]):
+    boxes = yolo_net_out_to_car_boxes(prediction[i], threshold=0.17)
+    box = draw_box(boxes, images[i], [[500, 1280], [300, 650]])
+    plt.imsave('../output_images/' + 'test' + str(i) + ' result.jpg', box)
